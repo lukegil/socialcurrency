90ba653788f01862e58350155105fcf3f0918a51
Luke Gilson <luke.gilson@gmail.com>
2016-10-12 12:24:56 -0400

    added close button

--- socialcurrency-plugin/templates/success-screen.php
+++ socialcurrency-plugin/templates/success-screen.php
@@ -1,6 +1,9 @@
 <?php
 
 return '<div class="scrr-success">
+    <div class="scrr-top-row">
+        <span class="scrr-close">&#10005;</span>
+    </div>
     <div class="scrr-success--txt">
         <p>Thanks for sharing!</p>
         <p>Enjoy your week!</p>
